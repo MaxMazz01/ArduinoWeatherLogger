5e8d46495cea275be63f47d3042e1d2d9bdc6070
MaxMazz01 <maxbot36@gmail.com>
2017-05-08 10:48:01 -0400

    Create Weather Station V1

---
+++ AWS_Rev1.ino
@@ -0,0 +1,105 @@
+// Ethernet2 - Version: Latest 
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet2.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp2.h>
+#include <Twitter.h>
+#include <util.h>
+
+// ThingSpeak - Version: Latest 
+#include <ThingSpeak.h>
+
+
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress ip(192, 168, 1, 177);
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+// (port 80 is default for HTTP):
+EthernetServer server(80);
+EthernetClient client;
+
+// Information for writing to a Thingspeak channel
+
+unsigned long myChannelNumber = 31461;
+const char * myWriteAPIKey = "LD79EOAAWRVYF04Y";
+unsigned long time;
+
+
+
+void setup() {
+  // Open serial communications and wait for port to open, as well as open Thingspeak
+  time = millis();
+  ThingSpeak.begin(client);
+  Serial.begin(9600);
+
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+}
+
+
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // output the value of each analog input pin
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel);
+            client.print("analog input ");
+            client.print(analogChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");
+          }
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+  else if (millis()-time > 900000){
+    temp = (4.64*analogRead(A0)*100)/1024;
+     ThingSpeak.writeField(myChannelNumber, 1, temp, myWriteAPIKey);
+     time = millis();
+  }
+  }
+}
